a1d40dac2136326226343dff53514c10736deeea
DevFaisal <dfaisal59@gmail.com>
2024-02-01 17:15:08 +0530

    Refactor Dashboard component layout

--- FrontEnd/src/Pages/Dashboard.jsx
+++ FrontEnd/src/Pages/Dashboard.jsx
@@ -8,15 +8,8 @@ function Dashboard() {
     return (
         <>
 
-            <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                <span className="sr-only">Open sidebar</span>
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                </svg>
-            </button>
-
-            <aside className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-orange-200 dark:bg-gray-800">
+            <aside className="fixed top-0 left-0 w-64 h-screen" >
+                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-800">
                     <ul className="space-y-2 font-medium">
                         <li>
                             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
@@ -62,7 +55,7 @@ function Dashboard() {
             </aside>
 
             <div className="p-4 sm:ml-64">
-                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                <div className="p-4 dark:border-gray-700">
                     <div className="grid grid-cols-3 gap-4 mb-4">
                         <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
                             <span className='text-2xl text-white'><Balance /></span>
